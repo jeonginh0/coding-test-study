bd29941d6b692bc383d21aeea4c7c3a5b751e990
정인호 <jeonginho@jeong-inhoui-MacBookAir.local>
2025-08-14 14:42:23 +0900

    Add: HashSet 2 Array

---
+++ src/beakjun/day8/MySolution.java
@@ -0,0 +1,37 @@
+package beakjun.day8;
+
+import java.util.*;
+
+public class MySolution {
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> intersectionSet = new HashSet<>();
+
+        for (int num: nums1){
+            set.add(num);
+        }
+
+        for (int num: nums2){
+            if(set.contains(num)){
+                intersectionSet.add(num);
+            }
+        }
+
+        int[] result = new int[intersectionSet.size()];
+        int i = 0;
+        for (int num: intersectionSet){
+            result[i++] = num;
+        }
+
+        Arrays.sort(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,2,1};
+        int[] nums2 = {2,2};
+
+        int[] result = intersection(nums1, nums2);
+        System.out.println(Arrays.toString(result));
+    }
+}
---
+++ src/beakjun/day8/SeniorSolution.java
@@ -0,0 +1,27 @@
+package beakjun.day8;
+
+import java.util.*;
+
+public class SeniorSolution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        Set<Integer> result = new HashSet<>();
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                result.add(num);
+            }
+        }
+
+        int[] answer = new int[result.size()];
+        int i = 0;
+        for (int num : result) {
+            answer[i++] = num;
+        }
+
+        return answer;
+    }
+}
